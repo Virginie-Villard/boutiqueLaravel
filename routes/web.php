59837250b70c3a03acbb716e14d36d5daa33c63b
Virginie Villard <virginie.villard@le-campus-numerique.fr>
2022-03-01 10:10:21 +0100

    Modification Route Home page.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\CartController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return 'Home page';
-});
+Route::get('/user', [HomeController::class, 'show']);
 
 Route::get('/products', function () {
     return 'Products list';
