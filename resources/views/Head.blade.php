420a44e068fbdf87d51e59bbbb0c7139b310ac7f
Virginie Villard <virginie.villard@le-campus-numerique.fr>
2022-03-01 12:23:16 +0100

    Change Template in Head

---
+++ resources/views/Head.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+{{-- @see : https://www.cloudways.com/blog/create-laravel-blade-layout/ --}}
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title> @yield('title') </title>
+</head>
+
+
+
