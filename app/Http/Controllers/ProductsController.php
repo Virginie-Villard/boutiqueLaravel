085b733eb066e7ec9211b33549354bf139d33c8c
Virginie Villard <virginie.villard@le-campus-numerique.fr>
2022-03-03 13:46:32 +0100

    Display Product / WIP display Products list

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,25 +2,100 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class ProductsController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display products
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function show()
     {
         $products = DB::table('products')->get();
         return view('Products', ['products'=>$products]);
     }
 
+    /**
+     * Display product by id
+     *
+     * @param $id
+     * @return \Illuminate\Http\Response
+     *
+     * @see : https://blog.quickadminpanel.com/5-ways-to-use-raw-database-queries-in-laravel/
+     * @see : https://laravel.com/docs/4.2/queries#selects !!!
+     */
     public function showProduct($id)
     {
-        // @see : https://blog.quickadminpanel.com/5-ways-to-use-raw-database-queries-in-laravel/
-        // @see : https://laravel.com/docs/4.2/queries#selects !!!
+        $product = Product::find($id);
 
-        $productById = DB::table('products')->where('id', $id)->first();
-        return view('Product', ['product'=>$productById]);
+        return view('Product', ['product'=>$product]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
-
-
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'image'
+    ];
 }
