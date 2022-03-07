25fa8e4b4118817e59fe744c8fe35e1aa088e881
Virginie Villard <virginie.villard@le-campus-numerique.fr>
2022-03-07 10:07:31 +0100

    Backoffice.blade form

--- resources/views/Backoffice.blade.php
+++ resources/views/Backoffice.blade.php
@@ -4,4 +4,22 @@
 
 @section('content')
     <h1>Product manager</h1>
+
+    <div class=formBackoffice>
+
+        <label for="name">Product name :</label>
+        <input type="text" id="name" name="name" required
+              minlength="1" maxlength="255"> <br />
+
+       <label for="description">Product description :</label>
+       <input type="text" id="description" name="description" required> <br />
+
+       <label for="price">Product price :</label>
+       <input type="text" id="price" name="price" required> <br />
+
+       <label for="image">Product image URL :</label>
+       <input type="text" id="image" name="image" required> <br />
+
+    </div>
+
 @stop
