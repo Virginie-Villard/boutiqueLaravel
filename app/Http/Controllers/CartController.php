c2f3a42b57948d7c53c5542ace3fa50fd76e21d1
Virginie Villard <virginie.villard@le-campus-numerique.fr>
2022-03-11 13:56:03 +0100

    Add Controller with some functions to Cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,13 +2,121 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 
 class CartController extends Controller
 {
-    public function show()
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
     {
-        return view('Cart');
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+//        \Cart::add([
+//            'id' => $request->id,
+//            'name' => $request->name,
+//            'price' => $request->price,
+//            'quantity' => $request->quantity,
+//            'attributes' => array(
+//                'image' => $request->image,
+//            )
+//        ]);
+//        session()->flash('success', 'Product is Added to Cart Successfully !');
+//
+//        return redirect()->route('cart.list');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Cart  $cart
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cart $cart)
+    {
+        $cartItems = \Cart::getContent();
+        // dd($cartItems);
+        return view('cart', compact('cartItems'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Cart  $cart
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Cart  $cart
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cart $cart)
+    {
+//        \Cart::update(
+//            $request->id,
+//            [
+//                'quantity' => [
+//                    'relative' => false,
+//                    'value' => $request->quantity
+//                ],
+//            ]
+//        );
+//
+//        session()->flash('success', 'Item Cart is Updated Successfully !');
+//
+//        return redirect()->route('cart.list');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Cart  $cart
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cart $cart)
+    {
+//        \Cart::remove($request->id);
+//        session()->flash('success', 'Item Cart Remove Successfully !');
+
+//        return redirect()->route('cart.list');
+    }
+
+    public function clearAll()
+    {
+//        \Cart::clear();
+//
+//        session()->flash('success', 'All Item Cart Clear Successfully !');
+//
+//        return redirect()->route('cart.list');
     }
 }
